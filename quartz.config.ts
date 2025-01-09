079b6b856e686ec6a73ccab8bccd6deb8404a3df
olblinov <ol.blinov@gmail.com>
2025-01-09 16:29:23 +0100

    Quartz sync: Jan 9, 2025, 4:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,11 +12,9 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "olblinov.github.io/privacy-news",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
